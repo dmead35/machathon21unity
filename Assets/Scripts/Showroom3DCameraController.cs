7daf5aad3905311114cb62f3169115b486163d7e
David Mead <david_mead@epam.com>
2021-09-16 02:27:29 -0400

    3D Tent Sale Scene

---
+++ Assets/Scripts/Showroom3DCameraController.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+
+public class Showroom3DCameraController : MonoBehaviour
+{
+    public Showroom3DInputController inputController;
+    protected const float rotationScaleInBarrelRollRotationDegreesPerInputValue = 40.0f;
+    protected const float rotationScaleInYawRotationDegreesPerInputValue = 40.0f;
+    protected const float rotationScaleInPitchRotationDegreesPerInputValue = 40.0f;
+
+    Vector2 rotation = Vector2.zero;
+    public float speed = 3;
+
+    protected enum CameraMovementModes
+    {
+        DoNothing = 0,
+        Pitch = 1,
+        Yaw = 2,
+        BarrelRoll = 3
+    }
+
+    void Update()
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        transform.eulerAngles = (Vector2)rotation * speed;
+        // UpdateCameraDirection();
+    }
+
+    protected void UpdateCameraDirection()
+    {
+        var cameraMovementModeSelection = DetermineWhichCameraMovementModeToHonor();
+
+        switch (cameraMovementModeSelection)
+        {
+            case CameraMovementModes.BarrelRoll:
+                var currentMouseX = inputController.CurrentMouseX;
+                var degreesToRotateZ = currentMouseX * rotationScaleInBarrelRollRotationDegreesPerInputValue;
+                BarrelRollCamera(degreesToRotateZ);
+                break;
+            case CameraMovementModes.Yaw:
+                currentMouseX = inputController.CurrentMouseX;
+                var degreesToRotateX = currentMouseX * rotationScaleInYawRotationDegreesPerInputValue;
+                YawCamera(degreesToRotateX);
+                break;
+            case CameraMovementModes.Pitch:
+                var currentMouseY = inputController.CurrentMouseY;
+                var degreesToPitchY = currentMouseY * rotationScaleInPitchRotationDegreesPerInputValue;
+                PitchCamera(degreesToPitchY);
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    protected CameraMovementModes DetermineWhichCameraMovementModeToHonor()
+    {
+        var returnValue = CameraMovementModes.DoNothing;
+
+        if (inputController.CurrentRightMouseButtonClicked)
+        {
+            // right click mode dominates everything
+            returnValue = CameraMovementModes.BarrelRoll;
+        }
+        else
+        {
+            var isXDisplacementBiggerThanYDisplacement = false;
+
+            var xDisplacement = Mathf.Abs(inputController.CurrentMouseX);
+            var yDisplacement = Mathf.Abs(inputController.CurrentMouseY);
+
+            if (xDisplacement > yDisplacement)
+            {
+                isXDisplacementBiggerThanYDisplacement = true;
+            }
+
+            if (isXDisplacementBiggerThanYDisplacement)
+            {
+                returnValue = CameraMovementModes.Yaw;
+            }
+            else
+            {
+                returnValue = CameraMovementModes.Pitch;
+            }
+        }
+
+        return returnValue;
+    }
+
+
+    protected void BarrelRollCamera(float howMuch)
+    {
+        // switch positive to negative
+        howMuch *= -1.0f;
+        transform.Rotate(Vector3.forward, howMuch, Space.Self);
+    }
+
+    protected void PitchCamera(float howMuch)
+    {
+        // switch positive to negative
+        howMuch *= -1.0f;
+        transform.Rotate(Vector3.right, howMuch, Space.Self);
+    }
+
+    protected void YawCamera(float howMuch)
+    {
+        transform.Rotate(Vector3.up, howMuch, Space.Self);
+    }
+}
---
+++ Assets/Scripts/Showroom3DInputController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Showroom3DInputController : MonoBehaviour
+{
+    public float CurrentMouseX;
+    public float CurrentMouseY;
+    public bool CurrentRightMouseButtonClicked;
+
+    // intended for Quit?, but not required
+    public bool EscapeKeyWasPushed;
+
+    public bool Enabled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentRightMouseButtonClicked = false;
+        EscapeKeyWasPushed = false;
+        Enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // only update sensed inputs if we are enabled
+        if (Enabled)
+        {
+            CurrentMouseX = Input.GetAxisRaw("Mouse X");
+            CurrentMouseY = Input.GetAxisRaw("Mouse Y");
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                CurrentRightMouseButtonClicked = true;
+            }
+            if (Input.GetMouseButtonUp(1))
+            {
+                CurrentRightMouseButtonClicked = false;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                EscapeKeyWasPushed = true;
+            }
+
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+                EscapeKeyWasPushed = false;
+            }
+        }
+    }
+}
